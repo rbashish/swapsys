3d23e200b121a52a6a52e7b3adf66bd4da81efc4
RB ASHISH <message.info1@gmail.com>
2023-03-08 23:18:45 +0530

    initial push

--- README.md
+++ README.md
@@ -1,22 +1,20 @@
-# swapsys - a linux cmd line utility program to manage swap space
-A Python package to add swap space to a Linux system
+# swapsys - a linux cmd line utility to manage swap space
 
-![GitHub language count](https://img.shields.io/github/languages/count/rbashish/add-swap-space?style=plastic)
-![PyPI - Python Version](https://img.shields.io/pypi/pyversions/add-swap-space?style=plastic)
-![PyPI - Wheel](https://img.shields.io/pypi/wheel/add-swap-space?style=plastic)
+![GitHub language count](https://img.shields.io/github/languages/count/rbashish/swapsys?style=plastic)
+![PyPI - Python Version](https://img.shields.io/pypi/pyversions/swapsys?style=plastic)
+![PyPI - Wheel](https://img.shields.io/pypi/wheel/swapsys?style=plastic)
 
-
-
-![GitHub issues](https://img.shields.io/github/issues/rbashish/add-swap-space?style=plastic)
-![PyPI - License](https://img.shields.io/pypi/l/add-swap-space?style=plastic)
-![PyPI - Downloads](https://img.shields.io/pypi/dm/add-swap-space?color=orange&label=PyPI%20downloads&style=plastic)
+![GitHub issues](https://img.shields.io/github/issues/rbashish/swapsys?style=plastic)
+![PyPI - License](https://img.shields.io/pypi/l/swapsys?style=plastic)
+![PyPI - Downloads](https://img.shields.io/pypi/dm/swapsys?color=orange&label=PyPI%20downloads&style=plastic)
 
 
 Description
 -----
-This package provides a command-line interface to add swap space to a Linux system.
+This package provides a command-line interface to manage swap space to a Linux system.
 
 It prompts the user for the desired swap size in GB and then adds the swap space using the dd and mkswap commands. After the swap space has been added, it updates `/etc/fstab` to make the swap permanent.
+It also has an options of removing, increasing and decreasing the memory size.
 
 Download & Install
 -----
@@ -24,18 +22,18 @@ Download & Install
 pip install swapsys
 ```
 
-`Note:` If pip is unrecognized try pip3 instead. 
+`Note:` Please note that some operating systems might be equipped with the python3 and pip3 commands instead of python and pip. 
 
 Usage
 ----
 ```
-sudo python3 -m swapsys
+swapsys
 ```
-`Note:` Make sure you have pip install under root user as updating /etc/fstab requires root permission.
+`Note:` Make sure you provide full path of swapsys (`which swapsys` to get the path) with sudo or run with root user as updating /etc/fstab requires root permission.
 
 Screenshot
 ----
-![pypi-add-swap-space.png](https://github.com/rbashish/add-swap-space/blob/main/image/pypi-add-swap-space.png)
+![pypi-swapsys.png](https://raw.githubusercontent.com/rbashish/swapsys/main/image/swapsys.png)
 
 Feedback/Issue
 ----
---
+++ src/swapsys.py
@@ -0,0 +1,87 @@
+import os
+import sys
+
+# Define the ANSI escape code for color
+RED = "\033[1;31m"
+GREEN = "\033[32m"
+
+def main():
+    print(r"""
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    
+███████╗██╗    ██╗ █████╗ ██████╗ ███████╗██╗   ██╗███████╗
+██╔════╝██║    ██║██╔══██╗██╔══██╗██╔════╝╚██╗ ██╔╝██╔════╝
+███████╗██║ █╗ ██║███████║██████╔╝███████╗ ╚████╔╝ ███████╗
+╚════██║██║███╗██║██╔══██║██╔═══╝ ╚════██║  ╚██╔╝  ╚════██║
+███████║╚███╔███╔╝██║  ██║██║     ███████║   ██║   ███████║
+╚══════╝ ╚══╝╚══╝ ╚═╝  ╚═╝╚═╝     ╚══════╝   ╚═╝   ╚══════╝
+                                                           
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+SWAPSYS - A python package to manage swap space in a Linux system.
+Copyright (c) 2023 - SWAPSYS DEV
+Visit https://pypi.org/swapsys to report any issue.                                         
+
+    """)
+    while True:
+        print("=============================")
+        print("Choose an option to continue:")
+        print("=============================")
+        print("1. Add swap memory")
+        print("2. Remove swap memory")
+        print("3. Increase swap memory")
+        print("4. Decrease swap memory")
+        print("5. Exit")
+        print("=============================")
+        choice = input("Enter your choice: ")
+        if choice == '1':
+            add_swap()
+        elif choice == '2':
+            remove_swap()
+        elif choice == '3':
+            increase_swap()
+        elif choice == '4':
+            decrease_swap()
+        elif choice == '5':
+            break
+        else:
+            print(f"{RED}Invalid choice!!\033[0m")
+
+def add_swap():
+    size = int(input("Enter the size of the swap to be added in GB: "))
+    os.system(f"sudo fallocate -l {size}G /swapfile")
+    os.system("sudo chmod 600 /swapfile")
+    os.system("sudo mkswap /swapfile")
+    os.system("sudo swapon /swapfile")
+    with open("/etc/fstab", "a") as f:
+        f.write("/swapfile swap swap defaults 0 0\n")
+
+def remove_swap():
+    confirm = input("Are you sure you want to remove the swap? (y/n): ")
+    if confirm.lower() == 'y':
+        os.system("sudo swapoff -v /swapfile")
+        os.system("sudo rm /swapfile")
+        with open("/etc/fstab", "r") as f:
+            lines = f.readlines()
+        with open("/etc/fstab", "w") as f:
+            for line in lines:
+                if "/swapfile" not in line:
+                    f.write(line)
+
+def increase_swap():
+    size = int(input("Enter the size to increase the swap in GB: "))
+    os.system(f"sudo swapoff -v /swapfile")
+    os.system(f"sudo dd if=/dev/zero of=/swapfile bs=1G count={size} oflag=append conv=notrunc")
+    os.system("sudo mkswap /swapfile")
+    os.system("sudo swapon /swapfile")
+
+def decrease_swap():
+    size = int(input("Enter the size to decrease the swap in GB: "))
+    os.system(f"sudo swapoff -v /swapfile")
+    os.system(f"sudo truncate -s -{size}G /swapfile")
+    os.system("sudo mkswap /swapfile")
+    os.system("sudo swapon /swapfile")
+
+if __name__ == "__main__":
+    main()
